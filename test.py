005601ac02bf9075bcb1c720bfb1411e82ded680
CamPlume1 <cam.k.plume@gmail.com>
2024-10-22 10:57:05 -0400

    Updating to pytest

--- test.py
+++ test.py
@@ -1,26 +1,21 @@
 # test_simple_methods.py
 
-import unittest
+import pytest
 from simple_methods import add, subtract, multiply, divide, square
 
-class TestSimpleMethods(unittest.TestCase):
+def test_add():
+    assert add(3, 4) == 7
 
-    def test_add(self):
-        self.assertEqual(add(3, 4), 7)
+def test_subtract():
+    assert subtract(10, 5) == 5
 
-    def test_subtract(self):
-        self.assertEqual(subtract(10, 5), 5)
+def test_multiply():
+    assert multiply(2, 3) == 6
 
-    def test_multiply(self):
-        self.assertEqual(multiply(2, 3), 6)
+def test_divide():
+    assert divide(10, 2) == 5
 
-    def test_divide(self):
-        self.assertEqual(divide(10, 2), 5)
-
-    def test_square(self):
-        # This test will fail deliberately (expected result is wrong)
-        self.assertEqual(square(5), 20)  # Should be 25, not 20
-
-if __name__ == '__main__':
-    unittest.main()
+def test_square():
+    # This test will fail deliberately (expected result is wrong)
+    assert square(5) == 20  # Should be 25, not 20
 
